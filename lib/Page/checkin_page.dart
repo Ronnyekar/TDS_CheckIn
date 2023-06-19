3e721cc34804d09da29a4adc620140a140188d9d
ronnyekar <ronnyekar@gmail.com>
2023-06-19 13:22:44 +0700

    First Commit

---
+++ lib/Page/checkin_page.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+Color firstcolor = const Color.fromARGB(255, 31, 56, 245);
+Color secondcolor = const Color.fromARGB(255, 30, 129, 242);
+Color secondcolor2 = Color.fromARGB(255, 197, 211, 227);
+Color thirdcolor = const Color.fromARGB(255, 255, 255, 255);
+
+class CheckinPage extends StatefulWidget {
+  const CheckinPage({super.key});
+
+  @override
+  State<CheckinPage> createState() => _CheckinPageState();
+}
+
+class _CheckinPageState extends State<CheckinPage> {
+  @override
+  Widget build(BuildContext context) {
+    final CheckinButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(100),
+      child: Container(
+        width: 330,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [firstcolor, secondcolor],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter),
+            borderRadius: const BorderRadius.all(Radius.circular(100))),
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          onPressed: () async {},
+          child: Text(
+            "Register",
+            style: TextStyle(
+                color: Theme.of(context).primaryColorLight,
+                fontWeight: FontWeight.bold,
+                fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white70.withOpacity(0.9),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        shadowColor: Colors.transparent,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [firstcolor, secondcolor],
+                end: Alignment.bottomCenter,
+                begin: Alignment.topCenter),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 25, left: 8, right: 8),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerLeft,
+                  width: AppBar().preferredSize.height + 40,
+                  height: AppBar().preferredSize.height,
+                  child: const Material(),
+                ),
+                const Expanded(
+                    flex: 8,
+                    child: Center(
+                      child: Text(
+                        'Check-In',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    )),
+                SizedBox(
+                  width: AppBar().preferredSize.height + 40,
+                  height: AppBar().preferredSize.height,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [secondcolor, thirdcolor])),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              flex: 8,
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60))),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Column(
+                        children: [
+                          const Text("No Registrasi"),
+                          const SizedBox(height: 10),
+                          TextFormField(
+                            autofocus: false,
+                            decoration: InputDecoration(
+                                contentPadding:
+                                    const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                                hintText: "",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(32))),
+                          ),
+                          Container(
+                            height: 60,
+                            width: 60,
+                            decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(60)),
+                                color: Color.fromARGB(255, 255, 74, 74)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: const <Widget>[
+                                // IconButton(
+                                //     onPressed: () {},
+                                //     icon: const Icon(
+                                //       Icons.network_ping_sharp,
+                                //       size: 30,
+                                //     )),
+                                Text(
+                                  "Check",
+                                  style: TextStyle(fontWeight: FontWeight.w500),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: Center(
+                      child: CheckinButton,
+                    )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Page/login_page.dart
@@ -0,0 +1,258 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:tds_check_in/Page/checkin_page.dart';
+import '../constant.dart';
+
+Color firstcolor = const Color.fromARGB(255, 31, 56, 245);
+Color secondcolor = const Color.fromARGB(255, 30, 129, 242);
+Color bgColor = const Color.fromARGB(255, 255, 255, 255);
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+bool _obscureText = true;
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController unameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final UsernameField = TextFormField(
+        controller: unameController,
+        autofocus: false,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Invalid Username';
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+            prefixIcon: const Icon(
+              Icons.email,
+            ),
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Username",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+
+    final passwordField = TextFormField(
+        controller: passwordController,
+        obscureText: _obscureText,
+        autofocus: false,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return 'Masukkan Password';
+          }
+          // Return null if the entered password is valid
+          return null;
+        },
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Password",
+            prefixIcon: const Icon(
+              Icons.vpn_key,
+            ),
+            suffixIcon: IconButton(
+              icon:
+                  Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+              onPressed: () {
+                setState(() {
+                  _obscureText = !_obscureText;
+                });
+              },
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0),
+            )));
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(100),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [firstcolor, secondcolor],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter),
+            borderRadius: const BorderRadius.all(Radius.circular(100))),
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          onPressed: () async {
+            if (_formKey.currentState!.validate()) {
+              Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                    builder: (context) => const CheckinPage(),
+                  ));
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Masukkan Input")));
+            }
+
+            // Navigator.push(
+            //     context,
+            //     CupertinoPageRoute(
+            //       builder: (context) => const CheckinPage(),
+            //     ));
+
+            // // if (_formKey.currentState!.validate()) {
+            //   dynamic result = await _auth.signInEmailPassword(
+            //       LoginUser(email: _email.text, password: _password.text));
+            //   if (result.uid == null) {
+            //     showDialog(
+            //         context: context,
+            //         builder: (context) {
+            //           return AlertDialog(
+            //             content: Text(result.code),
+            //           );
+            //         });
+            //   }
+            // }
+          },
+          child: Text(
+            "Log in",
+            style: TextStyle(
+                color: Theme.of(context).primaryColorLight,
+                fontWeight: FontWeight.bold,
+                fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: bgColor,
+      body: Container(
+        key: _formKey,
+        padding: EdgeInsets.only(bottom: 20),
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [firstcolor, secondcolor],
+                      end: Alignment.bottomCenter,
+                      begin: Alignment.topCenter),
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(100))),
+              child: Stack(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Image.asset(
+                      //   "images/Logo-white.png",
+                      //   height: 300,
+                      //   width: 300,
+                      // ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Form(
+              // key: _formKey,
+              child: Expanded(
+                flex: 1,
+                child: Container(
+                  margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      UsernameField,
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      passwordField,
+                      // Container(
+                      //     margin: const EdgeInsets.only(top: 10),
+                      //     alignment: Alignment.centerRight,
+                      //     child: Padding(
+                      //       padding: const EdgeInsets.symmetric(horizontal: 5),
+                      //       child: Row(
+                      //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      //         children: [
+                      //           Row(
+                      //             children: [
+                      //               Checkbox(
+                      //                 // value: rememberMe,
+                      //                 value: false,
+                      //                 onChanged: (value) {
+                      //                   // setState(() => rememberMe = value!);
+                      //                 },
+                      //                 activeColor: Colors.black26,
+                      //                 checkColor: Colors.black,
+                      //               ),
+                      //               Text(
+                      //                 'Rememeber Me ..',
+                      //                 style: kBodyText.copyWith(
+                      //                     color: Colors.black),
+                      //               ),
+                      //             ],
+                      //           ),
+                      //           GestureDetector(
+                      //             onTap: () {},
+                      //             child: Text(
+                      //               'Forgot Password?',
+                      //               style:
+                      //                   kBodyText.copyWith(color: Colors.black),
+                      //             ),
+                      //           ),
+                      //         ],
+                      //       ),
+                      //     )),
+                      const Spacer(),
+                      // Row(
+                      //   mainAxisAlignment: MainAxisAlignment.center,
+                      //   children: [
+                      //     Text(
+                      //       'New?',
+                      //       overflow: TextOverflow.ellipsis,
+                      //       style: kBodyText.copyWith(color: Colors.black),
+                      //     ),
+                      //     TextButton(
+                      //         onPressed: () {
+                      //           // widget.toggleView!();
+                      //         },
+                      //         child: Text(
+                      //           'Register Here',
+                      //           overflow: TextOverflow.ellipsis,
+                      //           style: kBodyText.copyWith(
+                      //               color: Colors.black,
+                      //               fontWeight: FontWeight.bold),
+                      //         )),
+                      //   ],
+                      // ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      loginButton
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
